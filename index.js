ff7cb9db3546ba4163ffd94ead068310b101a986
Gus Caplan <me@gus.host>
2017-11-02 14:00:00 +0000

    make better

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-module.exports = function sse(response) {
-  const headers = {
-    'Content-Type': 'text/event-stream',
-    'Cache-Control': 'no-cache',
-  };
+const headers = {
+  'Content-Type': 'text/event-stream',
+  'Cache-Control': 'no-cache',
+};
 
+module.exports = function sse(response) {
   if (response.respond) {
     headers[':status'] = 200;
     response.respond(headers);
@@ -16,13 +16,17 @@ module.exports = function sse(response) {
 
   return {
     send(event, data, id) {
-      if (id !== undefined) response.write(`id: ${id}\n`);
+      if (id !== undefined)
+        response.write(`id: ${id}\n`);
       response.write(`event: ${event}\n`);
-      if (!data) return;
+      if (!data)
+        return;
       const lines = String(data).replace(/\r(\n)?/g, '\n').split('\n');
       for (let i = 0; i < lines.length; i++) {
-        if (i + 1 === lines.length) response.write(`data: ${lines[i]}\n\n`);
-        else response.write(`data: ${lines[i]}\n`);
+        if (i + 1 === lines.length)
+          response.write(`data: ${lines[i]}\n\n`);
+        else
+          response.write(`data: ${lines[i]}\n`);
       }
       return true;
     },
